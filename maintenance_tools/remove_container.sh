62526c6a35a8c293ce85b38e3eac2c8f0e5f5978
marioalvesluck <marioalvesrzti@gmail.com>
2024-11-17 11:11:37 -0300

    Melhorias docker-compose-bridge

--- maintenance_tools/remove_container.sh
+++ maintenance_tools/remove_container.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
+# Função para exibir mensagens com data e hora
+log() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+log "Iniciando limpeza completa do ambiente Docker e pastas específicas..."
+
 # Remover todos os contêineres
-echo "Removendo todos os contêineres..."
-docker rm -f $(docker ps -aq)
+log "Removendo todos os contêineres..."
+docker ps -aq | xargs -r docker rm -f
 
 # Remover todas as imagens
-echo "Removendo todas as imagens..."
-docker rmi -f $(docker images -q)
+log "Removendo todas as imagens..."
+docker images -q | xargs -r docker rmi -f
 
 # Remover todos os volumes
-echo "Removendo todos os volumes..."
-docker volume rm $(docker volume ls -q)
+log "Removendo todos os volumes..."
+docker volume ls -q | xargs -r docker volume rm
 
 # Remover todas as redes personalizadas, ignorando redes predefinidas
-echo "Removendo todas as redes personalizadas..."
-docker network rm $(docker network ls --filter "type=custom" -q)
+log "Removendo todas as redes personalizadas..."
+docker network ls --filter "type=custom" -q | xargs -r docker network rm
 
-# Remover pastas em /var/lib/zabbix/*
-#echo "Removendo pastas em /var/lib/zabbix/*..."
-#rm -rf /var/lib/zabbix/*
+# Realizar uma limpeza geral usando docker system prune
+log "Executando docker system prune para limpeza geral..."
+docker system prune -af --volumes
 
-echo "Limpeza do Docker e das pastas do Zabbix concluída com sucesso."
+# Limpar pastas específicas (opcional)
+log "Removendo pastas em /var/lib/zabbix/*..."
+rm -rf /var/lib/zabbix/* || log "Nenhuma pasta encontrada para remover em /var/lib/zabbix/"
+
+log "Limpeza completa do Docker e do ambiente concluída com sucesso!"
